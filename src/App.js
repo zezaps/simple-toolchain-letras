536cb97bd39c248c4e54f08011a55d273b0079db
zezaps <anguind@gmail.com>
2020-04-30 15:24:31 +0200

    Sesion 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from './componentes/diseño/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/componentes/diseño/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav ClassName="navbar navbar-dark bg-dark mb-5">
+            <span className="navbar-brand mb-0 h1 mx-auto">Buscador de Letras</span>
+        </nav>);
+};
+
+export default Navbar
+
+
+
